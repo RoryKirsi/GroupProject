8587d3264aee842b082c86b72dccc002e1f56aaf
RoryKirsi <qq441072110@gmail.com>
2016-09-16 11:15:20 +1000

    Initial check-in of module SIT313GroupProject2

---
+++ SIT313GroupProject2/SIT313GroupProject2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SIT313GroupProject2
+{
+	public partial class SIT313GroupProject2Page : ContentPage
+	{
+		public SIT313GroupProject2Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
